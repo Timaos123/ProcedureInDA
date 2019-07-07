24c32bbe7c80cc7f855528567a9fa15b085d4985
KGLEE666 <41152222+KGLEE666@users.noreply.github.com>
2019-07-07 16:40:03 +0800

    Update A1_preprocessingForML.py

--- A1_preprocessingForML.py
+++ A1_preprocessingForML.py
@@ -14,16 +14,9 @@ def getTimeItemData():
     timeItemDf.to_csv("data/timeItemData.csv")
     print("finished")
 
- def getItemUserData():  
-    '''获取 商品-用户 数据'''
-    trainDf = pd.read_csv("data/Antai_AE_round1_train_20190626.csv")
-    itemList=list(set(np.array(trainDf["item_id"]).tolist()))
-    itemBuyerList=[[timeItem,",".join([str(intItem) for intItem in np.array(trainDf.loc[trainDf["item_id"]==timeItem,"buyer_admin_id"]).tolist()])] for timeItem in tqdm.tqdm(itemList)]
-    itemBuyerDf=pd.DataFrame(np.array(itemBuyerList),columns=["item_id","buyer_admin_id"])
-    itemBuyerDf.to_csv("data/itemBuyerData.csv")
-    print("finished")  
+
     
 if __name__=="__main__":
     getTimeItemData()
-    getItemUserData()
+
 
