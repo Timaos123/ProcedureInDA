42b1c04b85c1d7d9c4684ac2478512878404f664
Timaos123 <31198670+Timaos123@users.noreply.github.com>
2019-07-07 16:26:09 +0800

    Add files via upload

--- A1_preprocessingForML.py
+++ A1_preprocessingForML.py
@@ -1,1 +1,18 @@
+#coding:utf8
 
+import pandas as pd
+import numpy as np
+import tqdm
+
+def getTimeItemData():
+    '''获取 时间-商品 数据'''
+    trainDf=pd.read_csv("data/Antai_AE_round1_train_20190626.csv")
+    trainDf["create_order_time"]=trainDf["create_order_time"].apply(lambda x:x.split(" ")[0])
+    timeList=list(set(np.array(trainDf["create_order_time"]).tolist()))
+    timeItemList=[[timeItem,",".join([str(intItem) for intItem in np.array(trainDf.loc[trainDf["create_order_time"]==timeItem,"item_id"]).tolist()])] for timeItem in timeList]
+    timeItemDf=pd.DataFrame(np.array(timeItemList),columns=["create_order_time","item_id"])
+    timeItemDf.to_csv("data/timeItemData.csv")
+    print("finished")
+
+if __name__=="__main__":
+    getTimeItemData()
